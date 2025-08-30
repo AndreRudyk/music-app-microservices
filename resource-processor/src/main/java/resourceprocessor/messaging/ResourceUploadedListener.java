35f7120e3dc860ed78f1b4688951eb8069b9b04c
Andre <andrii.rudyk01@gmail.com>
2025-08-30 15:46:07 +0300

    Switching rabbitmq to stream

--- resource-processor/src/main/java/resourceprocessor/messaging/ResourceUploadedListener.java
+++ resource-processor/src/main/java/resourceprocessor/messaging/ResourceUploadedListener.java
@@ -1,20 +1,15 @@
 package resourceprocessor.messaging;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tika.exception.TikaException;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.http.ResponseEntity;
-import org.xml.sax.SAXException;
 import request.song.SongMetadataRequest;
 import resourceprocessor.feign.ResourceServiceClient;
 import resourceprocessor.feign.SongServiceClient;
 import resourceprocessor.service.CreateSongRequestService;
-
-import java.io.IOException;
+import java.util.function.Consumer;
 
 @Slf4j
 @Component
@@ -27,13 +22,20 @@ public class ResourceUploadedListener {
 
     private final ResourceServiceClient resourceServiceClient;
 
-    @RabbitListener(queues = "${spring.rabbitmq.queue}")
-    public void handleResourceUploaded(String resourceId) throws TikaException, IOException, SAXException {
-        log.info("Received a message with resourceId: {}", resourceId );
-        ResponseEntity<byte[]> response = resourceServiceClient.getResource(String.valueOf(resourceId));
-        byte[] resourceData = response.getBody();
-        SongMetadataRequest request = createSongRequestService.extractMetadata(resourceData,  Integer.valueOf(resourceId));
-        log.info("Before call song service with request: {}", request);
-        songServiceClient.saveSongMetadata(request);
+    @Bean
+    public Consumer<String> resourceUploaded() {
+        return resourceId -> {
+            try {
+                log.info("Received a message with resourceId: {}", resourceId);
+                ResponseEntity<byte[]> response = resourceServiceClient.getResource(resourceId);
+                byte[] resourceData = response.getBody();
+                SongMetadataRequest request =
+                        createSongRequestService.extractMetadata(resourceData, Integer.parseInt(resourceId));
+                log.info("Before calling song service with request: {}", request);
+                songServiceClient.saveSongMetadata(request);
+            } catch (Exception e) {
+                log.error("Error processing resource {}", resourceId, e);
+            }
+        };
     }
 }
--- resource-service/src/main/java/resourceservice/messaging/ResourceUploadedProducer.java
+++ resource-service/src/main/java/resourceservice/messaging/ResourceUploadedProducer.java
@@ -1,25 +1,16 @@
 package resourceservice.messaging;
 
-import lombok.AllArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ResourceUploadedProducer {
 
-    private final RabbitTemplate rabbitTemplate;
-
-    private final String queueName;
-
-    public ResourceUploadedProducer(RabbitTemplate rabbitTemplate,
-                                    @Value("${spring.rabbitmq.queue}") String queueName) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.queueName = queueName;
-    }
+    private final StreamBridge streamBridge;
 
     public void sendResourceId(String resourceId) {
-        rabbitTemplate.convertAndSend(queueName, resourceId);
+        streamBridge.send("resourceUploaded-out-0", resourceId);
     }
 }
