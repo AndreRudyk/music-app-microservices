33a93769ab9942f61ab9e76f943d6b67f9e70bd2
Andre <andrii.rudyk01@gmail.com>
2025-10-21 11:01:04 +0300

    Add authorization server (#9)

--- init-scripts/storage-db/init.sql
+++ init-scripts/storage-db/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS public;
 
 CREATE TABLE IF NOT EXISTS storage (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     storage_type VARCHAR(55),
     bucket VARCHAR(55),
     path VARCHAR(255)
